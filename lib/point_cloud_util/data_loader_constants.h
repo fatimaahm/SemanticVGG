b12bac26a27dd30e28d2c4392b3ecd5e72be37f4
fatimaahm <142170318+fatimaahm@users.noreply.github.com>
2023-09-24 13:07:30 +0100

    Update data_loader_constants.h

--- lib/point_cloud_util/data_loader_constants.h
+++ lib/point_cloud_util/data_loader_constants.h
@@ -1,8 +1,8 @@
 const int kWindowSize = 16;
 const int kBatchSize = 100;
 const int kNumberOfClasses = 8;
-const int kDefaultNumberOfScales = 5;
-const int kDefaultNumberOfRotations = 1;
+const int kDefaultNumberOfScales = 0;
+const int kDefaultNumberOfRotations = 0;
 const float kSpatialResolution = 0.025;
 const int kBatchResamplingLimit = 100;
 const bool kPointCloudVerbose = false;
